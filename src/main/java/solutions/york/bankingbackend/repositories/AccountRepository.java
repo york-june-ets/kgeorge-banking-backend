c3b3edd3ca05800fd88d3389abcfbb91c0164e32
Kayla George <kaylatrinitygeorge@gmail.com>
2025-07-17 16:42:44 -0400

    added findByAccountNumber to AccountRepository

--- src/main/java/solutions/york/bankingbackend/repositories/AccountRepository.java
+++ src/main/java/solutions/york/bankingbackend/repositories/AccountRepository.java
@@ -3,5 +3,8 @@ package solutions.york.bankingbackend.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import solutions.york.bankingbackend.models.Account;
 
+import java.util.Optional;
+
 public interface AccountRepository extends JpaRepository<Account, Long> {
+    public Optional<Account> findByAccountNumber(String accountNumber);
 }
